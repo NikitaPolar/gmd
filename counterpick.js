94be3a2d0e54ec34f08de4e97400551ff91b53e7
Nikita Polar <qwers@me.com>
2020-12-17 03:22:33 +0300

    Update counterpick.js

--- counterpick.js
+++ counterpick.js
@@ -1,19 +1,14 @@
 let exampleScript = {};
 
-exampleScript.OnScriptLoad = () => {
-    
-    // Проверяем находится ли клиент в игре, так как в ином случае будет возвращен null.
-    if(!GameRules.IsActiveGame()) {
-        return;
-    }
-    
-    // Получаем массив игроков
-    let players = EntitySystem.GetPlayersList();
-    
-    for(let player of players) {
-        // Выводим ник каждого игрока на карте
-        console.log(player.GetName());
-    }
-};
 
+exampleScript.OnUpdate = () => {
+		console.log('wait');
+		if(Engine.OnceAt(1)) {
+			let players = EntitySystem.GetPlayersList();
+			
+			for(let player of players) {
+				console.log(player.GetPlayerSelectedHeroName());
+			}
+		}
+};
 RegisterScript(exampleScript);
